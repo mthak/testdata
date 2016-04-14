17cb65b7e30408ae08ca17032880e527c24903c6
Manoj Thakkar <writetomanoj@gmail.com>
2016-04-14 05:44:16 +0000

    more changes for slave creation

--- bin/maven_install.sh
+++ bin/maven_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-wget http://mirrors.sonic.net/apache/maven/maven-3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+wget http://apache.spinellicreations.com/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 tar -zxf apache-maven-3.3.9-bin.tar.gz
 cp -R apache-maven-3.3.9 /usr/local
 ln -s /usr/local/apache-maven-3.3.9/bin/mvn /usr/bin/mvn
